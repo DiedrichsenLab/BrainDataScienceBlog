fdc318e4d93eafbacb58af5b1770112299ff58c0
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2021-10-02 22:57:37 -0400

    Figure 4 plottin

--- noisy_correlation/correlation_demo.py
+++ noisy_correlation/correlation_demo.py
@@ -1,9 +1,9 @@
-import numpy as np 
+import numpy as np
 import PcmPy as pcm
 from PcmPy import sim
 import pandas as pd
-import plotly.express as pe 
-import plotly.io as pio 
+import plotly.express as pe
+import plotly.io as pio
 import plotly.graph_objects as go
 
 def get_corr(X,cond_vec):
@@ -13,19 +13,19 @@ def get_corr(X,cond_vec):
 
 def get_noiseceil(X,cond_vec):
     rel = np.array([0.0,0.0])
-    for i in [0,1]: 
-        N = np.sum(cond_vec==i) # Number of measurements  
-        R = np.corrcoef(X[cond_vec==i,:]) # Correlation matrix 
-        index_R = np.where(~np.eye(N,dtype=bool)) # Average cross-block correlations 
-        r = np.mean(R[index_R]) # Average the non-diagnal elements. 
-        rel[i] = r * N / (r*(N-1)+1) # Overall realibility of the mean 
+    for i in [0,1]:
+        N = np.sum(cond_vec==i) # Number of measurements
+        R = np.corrcoef(X[cond_vec==i,:]) # Correlation matrix
+        index_R = np.where(~np.eye(N,dtype=bool)) # Average cross-block correlations
+        r = np.mean(R[index_R]) # Average the non-diagnal elements.
+        rel[i] = r * N / (r*(N-1)+1) # Overall realibility of the mean
 
     # Check if both are above zero
     if rel[0]>0 and rel[1]>0:
         noise_r = np.sqrt(rel[0]*rel[1])
-    else: 
-        noise_r = np.nan 
-    return noise_r 
+    else:
+        noise_r = np.nan
+    return noise_r
 
 
 def get_crossblock(X,cond_vec,part_vec):
@@ -38,14 +38,14 @@ def get_crossblock(X,cond_vec,part_vec):
     return crosscorr
 
 
-def do_sim(corr,signal=np.linspace(0,5,20),n_sim=50): 
+def do_sim(corr,signal=np.linspace(0,5,20),n_sim=50):
     M = pcm.CorrelationModel('corr',num_items=1,corr=corr,cond_effect=False)
     G,dG = M.predict([0,0])
     cond_vec,part_vec = pcm.sim.make_design(2,2)
     Lcorr = []
-    LnoiseCeil = [] 
+    LnoiseCeil = []
     Lsign = []
-    LcrossBlock =[] 
+    LcrossBlock =[]
     for s in signal:
         D = pcm.sim.make_dataset(M, [0,-0.5], cond_vec, n_sim=n_sim, signal=s)
         for i in range(n_sim):
@@ -59,9 +59,25 @@ def do_sim(corr,signal=np.linspace(0,5,20),n_sim=50):
     S['true'] = np.ones((S.shape[0],))*corr
     return S
 
+def do_sim_corrpcm(corr=0.7,signal=0.5,n_sim=20,nsteps = 11):
+    # Make the design in this case it's 2 runs, 2 conditions!
+    cond_vec,part_vec = pcm.sim.make_design(2,2)
+    # Generate different models from 0 to 1
+    M=[]
+    for r in np.linspace(0,1,nsteps):
+        M.append(pcm.CorrelationModel(f"{r:0.2f}",num_items=1,corr=r,cond_effect=False))
+    Mflex = pcm.CorrelationModel("flex",num_items=1,corr=None,cond_effect=False)
+    M.append(Mflex)
+    # For each simulation scenario, get different
+
+    Mtrue = pcm.CorrelationModel('corr',num_items=1,corr=corr,cond_effect=False)
+    D = pcm.sim.make_dataset(Mtrue, [0,0], cond_vec,part_vec=part_vec,n_sim=n_sim, signal=signal)
+    T,theta = pcm.inference.fit_model_individ(D,M,fixed_effect=None,fit_scale=False)
+    return T,theta,M
+
 def plot_Figure2(D,T,Tstd):
     # This code generates an interactive Figure for Figure2, using plotly
-    fig = go.Figure() 
+    fig = go.Figure()
 
     marker=dict(color='rgba(0, 200, 0, 0.04)', size=10)
     fig.add_scatter(x=D.signal,y=D.r_naive,
@@ -69,14 +85,14 @@ def plot_Figure2(D,T,Tstd):
                 marker=marker,hoverinfo='skip',
                 showlegend=False)
 
-    # Make the hover-template: Once that has been set, 
+    # Make the hover-template: Once that has been set,
     # hoverinfo does not have an effect any more
     hoverT = '<i>Signal</i>: %{x:.2f}<br>Mean: %{y:.2f}<br>%{text}'
-    # Make the text for each point 
+    # Make the text for each point
     text = []
     for s in Tstd['r_naive']:
         text.append(f"Std: {s:.2f}")
-    
+
     fig.add_scatter(x=T.signal, y=T.r_naive,
                 name='',
                 text = text,
@@ -87,7 +103,7 @@ def plot_Figure2(D,T,Tstd):
 
     hoverT = 'True correlation<br>%{y:.2f}'
 
-    fig.add_scatter(x=T.signal, y=T.true, 
+    fig.add_scatter(x=T.signal, y=T.true,
                 name='',
                 hovertemplate = hoverT,
                 line = dict(color='rgba(0, 0, 0, 1)', width=1, dash='dash'),
@@ -96,8 +112,8 @@ def plot_Figure2(D,T,Tstd):
 
 
     fig.update_layout(
-            hovermode='closest', 
-            autosize=True, # width =xx, heigh =xxx 
+            hovermode='closest',
+            autosize=True, # width =xx, heigh =xxx
             template = 'plotly_white',
             yaxis=dict(
                 title_text="Correlation",
@@ -112,7 +128,7 @@ def plot_Figure2(D,T,Tstd):
 
 def plot_Figure3(D,T,Tstd):
     # This code generates an interactive Figure for Figure2, using plotly
-    fig = go.Figure() 
+    fig = go.Figure()
 
     lines = ['r_naive','corr_corrected','corr_corrected_imp','cross_block','cross_block_imp','true']
     linestyle = [dict(color='rgba(0, 100, 0, 1)', width=3),
@@ -121,7 +137,7 @@ def plot_Figure3(D,T,Tstd):
                  dict(color='rgba(150, 0, 0, 1)', width=3),
                  dict(color='rgba(150, 0, 0, 1)', width=3,dash='dash'),
                  dict(color='rgba(0, 0, 0, 1)', width=1, dash='dash')]
-    shadecolor = ['rgba(0, 100, 0, 0.1)', 
+    shadecolor = ['rgba(0, 100, 0, 0.1)',
                    'rgba(0, 0, 150, 0.1)',
                    'rgba(0, 0, 150, 0.1)',
                    'rgba(150, 0, 0, 0.1)',
@@ -135,7 +151,7 @@ def plot_Figure3(D,T,Tstd):
             'True correlation<br>%{y:.2f}']
 
     for i,line in enumerate(lines):
-        # Make the text for each point 
+        # Make the text for each point
         text = []
         for s in Tstd[line]:
             text.append(f"Std: {s:.2f}")
@@ -162,8 +178,8 @@ def plot_Figure3(D,T,Tstd):
             showlegend=False)
 
     fig.update_layout(
-            hovermode='closest', 
-            autosize=True, # width =xx, heigh =xxx 
+            hovermode='closest',
+            autosize=True, # width =xx, heigh =xxx
             template = 'plotly_white',
             yaxis=dict(
                 title_text="Correlation",
@@ -175,6 +191,97 @@ def plot_Figure3(D,T,Tstd):
             )
         )
     return(fig)
+
+def plot_Figure4(T,theta,M):
+    # This code generates an interactive Figure for Figure4, using plotly
+    fig = go.Figure()
+
+    lines = []
+
+    markerstyle = [dict(color='rgba(0, 50, 50, 0.3)', size=7),
+                 dict(color='rgba(150, 0, 0, 1)', size=7)]
+    linestyle = [dict(color='rgba(0, 50, 50, 0.2)', width=2),
+                 dict(color='rgba(150, 0, 0, 1)', width=2)]
+    shadecolor = ['rgba(150, 0, 0, 0.1)']
+    hoverT = ['Maximum Likelihood%{x:.2f}',
+            'Mean Likelihood%{y:.2f}']
+
+    flexL = T.likelihood.iloc[:,-1].to_numpy()
+    flex_r = M[-1].get_correlation(theta[-1])
+    fixL = T.likelihood.iloc[:,0:-1].to_numpy()
+    n= fixL.shape[1]
+    fix_r = np.empty((n,))
+    for i in range(n):
+        fix_r[i]= M[i].corr
+    # recenter the correlation
+    mfixL = fixL.mean(axis=1)
+    fixL = fixL - mfixL.reshape(-1,1)
+    flexL = flexL - mfixL  # apply same scaling
+
+    # Plot individual linees
+    for i in range(fixL.shape[0]):
+        fig.add_scatter(x=fix_r, y=fixL[i,:],
+            name='',
+            line = linestyle[0],
+            mode = 'lines',
+            hoverinfo="skip",
+            showlegend=False)
+
+    # Plot the maximum
+    fig.add_scatter(x=flex_r, y=flexL,
+            name='',
+            marker = markerstyle[0],
+            mode = 'markers',
+            hovertemplate=hoverT[0],
+            showlegend=False)
+
+    # Now plot the mean line with
+    x = fix_r
+    y = fixL.mean(axis=0)
+    fig.add_scatter(x=fix_r, y=y,
+        name='',
+        line = linestyle[1],
+        hovertemplate=hoverT[1],
+        mode = 'lines',
+        showlegend=False)
+
+    # Do the maximal value of the mean
+    maxy=np.max(y)
+    maxr=x[np.argmax(y)]
+    fig.add_scatter(x=np.array([maxr]), y=np.array([maxy]),
+        name='',
+        marker = markerstyle[1],
+        hovertemplate='Max %{x:.2f}\n%{y:.2f}',
+        mode = 'markers',
+        showlegend=False)
+
+    s = fixL.std(axis=0)/np.sqrt(fixL.shape[0])
+    y_upper = y + s
+    y_lower = y - s
+    fig.add_scatter(x=np.concatenate([x,x[::-1]]), # x, then x reversed
+    y=np.concatenate([y_upper,y_lower[::-1]]), # upper, then lower reversed
+    fill='toself',
+    fillcolor=shadecolor[0],
+    line=dict(color='rgba(255,255,255,0)'),
+    hoverinfo="skip",
+    showlegend=False)
+
+    fig.update_layout(
+            hovermode='closest',
+            hoverdistance = 10,
+            autosize=True, # width =xx, heigh =xxx
+            template = 'plotly_white',
+            yaxis=dict(
+                title_text="Likelihood",
+                titlefont=dict(size=18)
+            ),
+            xaxis=dict(
+                title_text="Signal to Noise",
+                titlefont=dict(size=18)
+            )
+        )
+    return(fig)
+
 
 def dosim_2():
     sig = np.linspace(0.1,5.1,21)# np.logspace(np.log(0.2),np.log(5),10)
@@ -190,7 +297,7 @@ def dosim_2():
     fig = plot_Figure2(D,T,Tstd)
     fig.write_html("Figure_2.html",include_plotlyjs='cdn',full_html=False)
     fig.show()
-    
+
 def dosim_3():
     sig = np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.5,2,2.5,3,3.5,4])# np.logspace(np.log(0.2),np.log(5),10)
     D = do_sim(0.7,n_sim=200,signal=sig)
@@ -206,21 +313,15 @@ def dosim_3():
     fig.write_html("Figure_3.html",include_plotlyjs='cdn',full_html=False)
     fig.show()
 
-def do_sim_pcm(corr=[0.7,0.9,0.7],signal=[1,0.2,0.3],n_sim=10): 
-    # Make the design in this case it's 2 runs, 2 conditions!  
-    cond_vec,part_vec = pcm.sim.make_design(2,2)
-    # Generate different models from 0 to 1
-    M=[] 
-    for r in np.linspace(0,1,11): 
-        M.append(pcm.CorrelationModel(f"R{r:0.2f}",num_items=1,corr=r,cond_effect=False))
-    # For each simulation scenario, get different 
-    for i,r in enumerate(corr): 
-        Mtrue = pcm.CorrelationModel('corr',num_items=1,corr=r,cond_effect=False)
-        D = pcm.sim.make_dataset(Mtrue, [0,-0.5], cond_vec,part_vec=part_vec,n_sim=n_sim, signal=signal[i])
-        T = pcm.inference.fit_model_individ(D,M,run_effect='None')
-        pass
+def dosim_4():
+    T,theta,M = do_sim_corrpcm(corr=0.7,signal=0.5,n_sim=20,nsteps=21)
+    fig=plot_Figure4(T,theta,M)
+    fig.show()
+    pass
+
+
 
 
 if __name__ == "__main__":
-    do_sim_pcm()
+    dosim_4()
     pass
